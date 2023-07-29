f49cccac6d783a34974f6b0aa729ecfd4443ff89
izi4kasss <maksymromanyshyn@gmail.com>
2023-07-29 19:15:15 -0400

    Door animation, Added house

---
+++ outmind/Assets/Scripts/Doors.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator anim;
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            anim.SetTrigger("open");
+        }
+        
+    }
+}
